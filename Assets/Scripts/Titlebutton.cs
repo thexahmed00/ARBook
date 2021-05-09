9ad47ef78f22e6066dd2662bb9206e672b13e617
thexahmed00 <ma2k510@gmail.com>
2021-05-09 22:31:52 +0530

    final commit

---
+++ Assets/Scripts/Titlebutton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using  Vuforia;
+
+public class Titlebutton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Text;
+    private VirtualButtonBehaviour vrBehaviour;
+    public AudioSource sound;
+    void Start()
+    {
+        Text.SetActive(false);
+       // Boy.SetActive(false);
+        vrBehaviour = GetComponentInChildren<VirtualButtonBehaviour>();
+        vrBehaviour.RegisterOnButtonPressed(OnButtonPressed);
+        vrBehaviour.RegisterOnButtonReleased(OnButtonRelease);
+        //vrBehaviour.UnregisterOnButtonPressed(OnButtonPressedOff);
+    }
+
+    // Update is called once per f
+    public void OnButtonPressed(VirtualButtonBehaviour vrb)
+    {
+        sound.Play();
+        Text.SetActive(true);
+       // Boy.SetActive(true);
+    }
+    public void OnButtonRelease(VirtualButtonBehaviour vrb)
+    {
+        sound.Stop();
+        Text.SetActive(false);
+        // Boy.SetActive(true);
+    }
+
+}
